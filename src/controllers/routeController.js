95464f91587c627fa613a45654f54978cea64184
Ashik Meerankutty <ashik9591@gmail.com>
2019-08-13 17:40:02 +0530

    Added basic controller to get routes

---
+++ src/controllers/routeController.js
@@ -0,0 +1,5 @@
+const getRoute = (req,res) => {
+    console.log(req.body)
+}
+
+export default getRoute
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from 'express'
+import getRoute from '../controllers/routeController'
 
 const router = Router()
 
 router.get('/', (req, res) => res.send({"status":"working"}))
+router.post('/route',getRoute)
 
 
 export default router
