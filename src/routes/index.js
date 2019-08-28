5d49b510cbca113fef66ff448a726a62c8fd4816
Ashik Meerankutty <ashik9591@gmail.com>
2019-08-28 13:14:53 +0530

    Added getLocation function to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 import getRoute from '../controllers/routeController'
+import getLocation from '../controllers/locationController'
 
 const router = Router()
 
 router.get('/', (req, res) => res.send({"status":"working"}))
 router.post('/route',getRoute)
+router.get('/locations',getLocation)
 
 
 export default router
